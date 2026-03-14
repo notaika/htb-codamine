fdb01c8ae92c8be38edbbf1ce7b5d895da23c477
Steven Joo <stevenjoo@Stevens-MacBook-Pro.local>
2026-03-14 13:28:50 -0700

    add: new folder

--- src/extension.ts
+++ src/extension.ts
@@ -52,29 +52,28 @@ class BruceViewProvider implements vscode.WebviewViewProvider {
   }
 
   private _getHtmlForWebview(webview: vscode.Webview) {
-    const scriptUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "app", "main.js"),
-    );
-    const styleMainUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "styles", "main.css"),
-    );
-    const nonce = getNonce();
+  const scriptUri = webview.asWebviewUri(
+    vscode.Uri.joinPath(this._extensionUri, "webview-ui", "dist", "assets", "index.js")
+  );
+  const styleMainUri = webview.asWebviewUri(
+    vscode.Uri.joinPath(this._extensionUri, "webview-ui", "dist", "assets", "index.css")
+  );
+  const nonce = getNonce();
 
     return `<!DOCTYPE html>
-			<html lang="en">
-			<head>
-				<meta charset="UTF-8">
-				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src \${webview.cspSource}; script-src 'nonce-\${nonce}';">
-				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-				<link href="\${styleMainUri}" rel="stylesheet">
-				<title>brucey Loosey</title>
-			</head>
-			<body>
-				<h1> bruce pls come back. </h1>
-				<button class="add-tear-button">+ 1 tear for Bruce</button>
-				<script nonce="\${nonce}" src="\${scriptUri}"></script>
-			</body>
-			</html>`;
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="${styleMainUri}" rel="stylesheet">
+      <title>Dopamine Feeder</title>
+    </head>
+    <body>
+      <div id="root"></div>
+      <script nonce="${nonce}" src="${scriptUri}"></script>
+    </body>
+    </html>`;
   }
 }
 
