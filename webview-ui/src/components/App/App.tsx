6f6ddcbdef07f1c44e3ae86deb3a5a8ba7088ccf
Arsen_Madi <arsen.madi08@gmail.com>
2026-03-15 05:29:33 -0700

    Message window

--- webview-ui/src/components/App/App.tsx
+++ webview-ui/src/components/App/App.tsx
@@ -1,58 +1,54 @@
-
 import "./App.css";
 import Summary from "../Summary/Summary";
+import { Sprite } from "../ForArsen/Arsen";
 import Arsen from "../ForArsen/Arsen";
 import { useState, useEffect } from "react";
 import XPBar from "../XPBar";
 import LinesWritten from "../LinesWritten/LinesWritten";
-import Sprite from "../Sprite/Sprite";
 
 declare function acquireVsCodeApi(): {
-  postMessage: (message: unknown) => void
-  getState: () => any
-  setState: (state: unknown) => void
-}
+  postMessage: (message: unknown) => void;
+  getState: () => any;
+  setState: (state: unknown) => void;
+};
 
-const vscode = acquireVsCodeApi()
+const vscode = acquireVsCodeApi();
 
 function App() {
-  const [xp, setXp] = useState(0)
+  const [xp, setXp] = useState(0);
 
   useEffect(() => {
     const handler = (event: MessageEvent) => {
-      const message = event.data 
+      const message = event.data;
 
-        console.log('message received:', message)
+      console.log("message received:", message);
 
       switch (message.type) {
-        case 'initializeBar':
+        case "initializeBar":
+          console.log("initializeBar fired, xp:", message.xp);
 
-          console.log('initializeBar fired, xp:', message.xp)
-
-          setXp(message.xp)
-          break
-        case 'updateXP':
-
-          console.log('updateXP fired, xp:', message.xp)
-          setXp(message.xp)
-          vscode.setState({xp: message.xp})
+          setXp(message.xp);
           break;
-        case 'levelUp':
-          console.log('levelup recieve', message.xpToNext)
-          break
+        case "updateXP":
+          console.log("updateXP fired, xp:", message.xp);
+          setXp(message.xp);
+          vscode.setState({ xp: message.xp });
+          break;
+        case "levelUp":
+          console.log("levelup recieve", message.xpToNext);
+          break;
       }
-    }
+    };
 
-    window.addEventListener('message', handler)
-    return() => window.removeEventListener('message', handler)
-  }, [])
+    window.addEventListener("message", handler);
+    return () => window.removeEventListener("message", handler);
+  }, []);
 
   return (
     <>
       <section id="panel" className="dashboard">
-        <Arsen />
         <Summary />
-        <Sprite />
+        <Arsen />
         <XPBar xp={xp} />
         <LinesWritten />
       </section>
